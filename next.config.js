123823bb8fa03be98db25615df4fa4344c27fd5f
hyun gyu <gusrb3164@gmail.com>
2021-05-21 11:12:08 +0900

    fix: fix api base url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
 		return [
 			{
 				source: '/api/:path*',
-				destination: 'http://localhost:5000/:path*',
+				destination: 'http://localhost:5000/api/:path*',
 			},
 		];
 	},
