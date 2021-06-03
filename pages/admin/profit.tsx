d070d1c528ae4790f02414ea2047888fb772c07f
hyun gyu <gusrb3164@gmail.com>
2021-06-03 20:53:19 +0900

    feat: add profit page

--- pages/admin/profit.tsx
+++ pages/admin/profit.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Layout } from '../../components/index';
+import { TextField, Input, Button } from '@material-ui/core';
 const profit = () => {
 	return (
 		<Layout>
-			<h1 id="movie-header">매출 조회</h1>
+			<h1 id="profit-header">매출 조회</h1>
+			<div>
+				<Input type="month"></Input>
+				<b>&nbsp;~&nbsp;</b>
+				<Input type="month"></Input>
+				&nbsp;
+				<Button variant="outlined" style={{ fontWeight: 'bold' }} color="primary">
+					조회
+				</Button>
+			</div>
 		</Layout>
 	);
 };
